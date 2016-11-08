40e1e732c529d21ab5f5f94a42a5f99edec5867b
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2016-11-08 13:24:48 +0000

    adding event id populator

--- src/Pop/Populator.php
+++ src/Pop/Populator.php
@@ -21,4 +21,12 @@ class Populator {
     $fields['contact_id_a'] = $this->entityStore->getRandom('Contact', array('contact_type' => $relationshipType['contact_type_a']))['id'];
     $fields['contact_id_b'] = $this->entityStore->getRandom('Contact', array('contact_type' => $relationshipType['contact_type_b']))['id'];
   }
+
+  function eventId($entity, &$fields){
+
+    // if no relationship type has been specified, get one
+    if(!isset($fields['event_id'])){
+      $fields['event_id'] = $this->entityStore->getRandom('Event', array('is_template' => false))['id'];
+    }
+  }
 }
